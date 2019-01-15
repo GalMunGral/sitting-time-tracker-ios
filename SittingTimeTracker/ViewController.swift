2659f1608ab75a4d9afbce678ef0e01c1f3a5b49
HoMoonKi <hewenqi96@gmail.com>
2019-01-15 00:51:06 -0500

    Small changes

--- SittingTimeTracker/ViewController.swift
+++ SittingTimeTracker/ViewController.swift
@@ -14,47 +14,38 @@ class ViewController: UIViewController {
   var motionManager: CMMotionManager?
   let queue = OperationQueue()
   var prevAcceleration = 0.0
+  
   @IBOutlet weak var displayText: UITextField!
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     self.motionManager = (UIApplication.shared.delegate as! AppDelegate).motionManager
-    startQueuedUpdates()
+    startQueuedUpdates() // Defined below
   }
   
   private func startQueuedUpdates() {
     guard let motionManager = motionManager else { return }
     motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
-    motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: self.queue) { (data, err) in
-      if let err = err {
-        print("Error: \(err.localizedDescription)")
-        return
+    motionManager.startDeviceMotionUpdates(
+      using: .xMagneticNorthZVertical, to: self.queue) { (data, err) in
+      if let err = err { print("Error: \(err.localizedDescription)"); return }
+      guard let data = data else { return }
+      
+      let acceleration = (
+        data.gravity.x * data.userAcceleration.x
+        + data.gravity.y * data.userAcceleration.y
+        + data.gravity.z * data.userAcceleration.z
+        ) / 9.81
+      if (acceleration.magnitude < 0.01) { return }
+      
+      if (self.prevAcceleration <= 0 && acceleration > 0) {
+        DispatchQueue.main.async { self.displayText.text = "아래" }
+      } else if (self.prevAcceleration >= 0 && acceleration < 0) {
+        DispatchQueue.main.async { self.displayText.text = "위" }
       }
       
-      if let data = data {
-        let acceleration = (
-          data.gravity.x * data.userAcceleration.x
-          + data.gravity.y * data.userAcceleration.y
-          + data.gravity.z * data.userAcceleration.z
-          ) / 9.81
-        if (acceleration.magnitude < 0.01) { return }
-        
-        var text: String!
-        if (self.prevAcceleration <= 0 && acceleration > 0) {
-          text = "아래"
-        } else if (self.prevAcceleration >= 0 && acceleration < 0) {
-          text = "위"
-        } else {
-          return
-        }
-        
-        // Update UI on main thread
-        DispatchQueue.main.async { self.displayText.text = text }
-        self.prevAcceleration = acceleration        
-      }
-      
+      self.prevAcceleration = acceleration
     }
     
   }

031abf54513b5f8a3ffa31d016ad149a68563c32
HoMoonKi <hewenqi96@gmail.com>
2019-02-05 00:45:47 -0500

    Add navigation stack

---
+++ SittingTimeTracker/HistoryViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HistoryViewController.swift
+//  SittingTimeTracker
+//
+//  Created by Wenqi He on 2/5/19.
+//  Copyright © 2019 Wenqi He. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HistoryViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, UIScrollViewDelegate {
+
+  var webView: WKWebView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "History"
+    let preferences = WKPreferences()
+    preferences.javaScriptEnabled = true
+    let configuration = WKWebViewConfiguration()
+    configuration.preferences = preferences
+    webView = WKWebView(
+      frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height),
+      configuration: configuration
+    )
+    view.addSubview(webView)
+    webView.uiDelegate = self
+    webView.navigationDelegate = self
+    webView.scrollView.delegate = self
+    webView.load(URLRequest(url: URL(string: "http://128.61.29.253:8080")!, cachePolicy: URLRequest.CachePolicy.reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 60.0))
+  }
+
+  func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+    print(error.localizedDescription)
+  }
+
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    print("Page loaded")
+  }
+
+  func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+    scrollView.pinchGestureRecognizer?.isEnabled = false
+  }
+
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      // Get the new view controller using segue.destination.
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
--- SittingTimeTracker/ViewController.swift
+++ SittingTimeTracker/ViewController.swift
@@ -7,45 +7,21 @@
 //
 
 import UIKit
-import WebKit
 import CoreMotion
 
-class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, UIScrollViewDelegate {
+class ViewController: UIViewController {
     
-  @IBOutlet weak var displayText: UITextField!
-  var webView: WKWebView!
-
+  
+  @IBOutlet weak var displayText: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.title = "Sitting Time Tracker"
     (UIApplication.shared.delegate as! AppDelegate).mainVC = self
   }
   
   @IBAction func viewHistory(_ sender: Any) {
-    let preferences = WKPreferences()
-    preferences.javaScriptEnabled = true
-    let configuration = WKWebViewConfiguration()
-    configuration.preferences = preferences
-    webView = WKWebView(
-      frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height),
-      configuration: configuration
-    )
-    view.addSubview(webView)
-    webView.uiDelegate = self
-    webView.navigationDelegate = self
-    webView.scrollView.delegate = self
-    webView.load(URLRequest(url: URL(string: "http://128.61.29.253:8080")!))
+    self.navigationController!.pushViewController(HistoryViewController(), animated: true)
   }
   
-  func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-    print(error.localizedDescription)
-  }
-  
-  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    print("Page loaded")
-  }
-  
-  func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-    scrollView.pinchGestureRecognizer?.isEnabled = false
-  }
-
 }
